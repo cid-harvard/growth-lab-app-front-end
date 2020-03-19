3021dc52eebe79deb92b9edae250d365a0eca76e
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-03-19 12:35:12 -0400

    Fix EOF

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,21 +2,21 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == 'backend' ] ; then
-  ssh ubuntu@$STAGING_IP_ADDRESS << EOF
-    sudo apt update
-    sudo apt install -y nginx
-    sudo systemctl enable nginx
-    mkdir -p /usr/local/nginx/conf/
-    mkdir -p /var/log/nginx/
-    sudo chmod -R 766 /var/log/nginx/
-  EOF
+	ssh ubuntu@$STAGING_IP_ADDRESS <<- EOF
+		sudo apt update
+		sudo apt install -y nginx
+		sudo systemctl enable nginx
+		mkdir -p /usr/local/nginx/conf/
+		mkdir -p /var/log/nginx/
+		sudo chmod -R 766 /var/log/nginx/
+	EOF
 
-  # Copy Travis build
-  rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/frontend && rsync" $TRAVIS_BUILD_DIR/build ubuntu@$STAGING_IP_ADDRESS:$APP_DIR/frontend
+	# Copy Travis build
+	rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/frontend && rsync" $TRAVIS_BUILD_DIR/build ubuntu@$STAGING_IP_ADDRESS:$APP_DIR/frontend
 
-  # Copy NGINX and Gunicorn configs
-  scp ./config/nginx.conf ubuntu@$STAGING_IP_ADDRESS:/usr/local/nginx/conf
-  scp ./config/gunicorn.ini ubuntu@$STAGING_IP_ADDRESS:$APP_DIR
+	# Copy NGINX and Gunicorn configs
+	scp ./config/nginx.conf ubuntu@$STAGING_IP_ADDRESS:/usr/local/nginx/conf
+	scp ./config/gunicorn.ini ubuntu@$STAGING_IP_ADDRESS:$APP_DIR
 else
-  echo "Branch not specified in script. Not deploying to server."
+	echo "Branch not specified in script. Not deploying to server."
 fi
