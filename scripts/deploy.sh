df3ec7face023029bd3d5f58a22b116fad5b6078
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-03-19 14:06:18 -0400

    Env variables based on branch name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -xe
 
-if [ $TRAVIS_BRANCH == 'backend' ] ; then
-	ssh ubuntu@$STAGING_IP_ADDRESS <<- EOF
-		sudo apt update
-		sudo apt install -y nginx
-		sudo systemctl enable nginx
-		sudo mkdir -p /etc/nginx
-		sudo chown -R ubuntu /etc/nginx
-		sudo chmod -R 755 /etc/nginx/
-		sudo mkdir -p /var/log/nginx/
-		sudo chown -R ubuntu /var/log/nginx
-		sudo chmod -R 755 /var/log/nginx/
-	EOF
+ssh $SERVER_USER@$IP_ADDRESS <<- EOF
+	sudo apt update
+	sudo apt install -y nginx
+	sudo systemctl enable nginx
+	sudo mkdir -p /etc/nginx
+	sudo chown -R $SERVER_USER /etc/nginx
+	sudo chmod -R 755 /etc/nginx/
+	sudo mkdir -p /var/log/nginx/
+	sudo chown -R $SERVER_USER /var/log/nginx
+	sudo chmod -R 755 /var/log/nginx/
+EOF
 
-	# Copy Travis build
-	rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/frontend && rsync" $TRAVIS_BUILD_DIR/build ubuntu@$STAGING_IP_ADDRESS:$APP_DIR/frontend
+# Copy Travis build
+rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/frontend && rsync" $TRAVIS_BUILD_DIR/build $SERVER_USER@$IP_ADDRESS:$APP_DIR/frontend
 
-	# Copy NGINX and Gunicorn configs
-	scp ./config/nginx.conf ubuntu@$STAGING_IP_ADDRESS:/etc/nginx
-	scp ./config/gunicorn.ini ubuntu@$STAGING_IP_ADDRESS:$APP_DIR
-else
-	echo "Branch not specified in script. Not deploying to server."
-fi
+# Copy NGINX and Gunicorn configs
+scp ./config/nginx.conf $SERVER_USER@$IP_ADDRESS:/etc/nginx
+scp ./config/gunicorn.ini $SERVER_USER@$IP_ADDRESS:$APP_DIR
