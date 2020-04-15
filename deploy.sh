051711eb31c702e944c92acdb10fc9cba0272262
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-04-15 17:15:06 -0400

    EC2 infrastructure with Travis CI (#1)
    * Test EC2 travis config
    
    * add npm run build script
    
    * Deploy config changes
    
    * move npm build from deploy to main body
    
    * missing IP address variable
    
    * Specify app directory on server
    
    * Move staging IP variable to console
    
    * Remove nginx copy for now
    
    * Move nginx/gunicorn copy to deploy.sh
    
    * Specify bash for script
    
    * Install and start nginx
    
    * Add -y to install nginx
    
    * Deal with nginx perms
    
    * Fix EOF
    
    * nginx chmod
    
    * sudo
    
    * move nginx files to /etc/nginx
    
    * change perm levels
    
    * recursive chown
    
    * Env variables based on branch name
    
    * Multi-environment deploy keys
    
    * change IP_ADDRESS to SERVER_ADDRESS
    
    * Environment conditional logic
    
    * Fix syntax
    
    * spaces to tabs
    
    * break if/elif into multiple statements
    
    * Remove branch restrictions
    
    * staging.enc to staging_key.enc
    
    * travis tabs to spaces
    
    * remove gunicorn from frontend
    
    * update to https
    
    * Remove cache
    
    * Nginx config and new encrypted ssh keys
    
    * New single SSH key encrypted
    
    * deploy all_branches: true
    
    * Move all script to deploy.sh
    
    * Alter nginx reset command

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -xe
+
+SERVER_ADDRESS=$1
+SERVER_USER=ubuntu
+APP_DIR=/home/ubuntu
+
+# Decrypt deploy key and add to SSH keys
+openssl aes-256-cbc -K $encrypted_ba9faf39c0a7_key -iv $encrypted_ba9faf39c0a7_iv -in deploy_key.enc -out ./deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
+
+ssh $SERVER_USER@$SERVER_ADDRESS <<- EOF
+	sudo apt update
+	sudo apt install -y nginx
+	sudo systemctl enable nginx
+	sudo mkdir -p /etc/nginx
+	sudo chown -R $SERVER_USER /etc/nginx
+	sudo chmod -R 755 /etc/nginx/
+	sudo mkdir -p /var/log/nginx/
+	sudo chown -R $SERVER_USER /var/log/nginx
+	sudo chmod -R 755 /var/log/nginx/
+EOF
+
+# Copy Travis build
+rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/frontend && rsync" $TRAVIS_BUILD_DIR/build $SERVER_USER@$SERVER_ADDRESS:$APP_DIR/frontend
+
+# Copy NGINX config
+scp ./config/nginx.conf $SERVER_USER@$SERVER_ADDRESS:/etc/nginx
+
+# Reset NGINX
+ssh $SERVER_USER@$SERVER_ADDRESS sudo systemctl reload nginx
