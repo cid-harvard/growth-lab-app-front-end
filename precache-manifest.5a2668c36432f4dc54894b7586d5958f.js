58a82f51c9f99d63c15b28cccdce41ee07b1bc90
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-15 20:52:03 +0000

    Deploy cid-harvard/country-tools-front-end to github.com/cid-harvard/country-tools-front-end.git:gh-pages

---
+++ precache-manifest.5a2668c36432f4dc54894b7586d5958f.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b5d672b52268f810536bf21146962e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0016593c442005614c3d",
+    "url": "./static/css/0.6a0208f7.chunk.css"
+  },
+  {
+    "revision": "56efe381f0f7efa2b58e",
+    "url": "./static/css/2.936d9420.chunk.css"
+  },
+  {
+    "revision": "1ab59cb48620e35ab841",
+    "url": "./static/css/7.936d9420.chunk.css"
+  },
+  {
+    "revision": "5aa69e7045e3774f9e9e",
+    "url": "./static/css/8.936d9420.chunk.css"
+  },
+  {
+    "revision": "011b9ade218109fb61c6",
+    "url": "./static/css/9.39db3a9c.chunk.css"
+  },
+  {
+    "revision": "210ddbb087a7574fb798",
+    "url": "./static/css/main.5b3b3896.chunk.css"
+  },
+  {
+    "revision": "0016593c442005614c3d",
+    "url": "./static/js/0.b7faa4fd.chunk.js"
+  },
+  {
+    "revision": "0027d4c1c96175d5abea",
+    "url": "./static/js/1.2ea49792.chunk.js"
+  },
+  {
+    "revision": "f46b2c115acdb647c0b3",
+    "url": "./static/js/10.3f60c3fd.chunk.js"
+  },
+  {
+    "revision": "9d6d096ffbdbeff731e22f5c1fde238e",
+    "url": "./static/js/10.3f60c3fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b33fbece38a50f70ce",
+    "url": "./static/js/11.be7deb1e.chunk.js"
+  },
+  {
+    "revision": "56efe381f0f7efa2b58e",
+    "url": "./static/js/2.66d5d892.chunk.js"
+  },
+  {
+    "revision": "31563de832c2b6ce2f61",
+    "url": "./static/js/5.90b70d31.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "./static/js/5.90b70d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909c6b171479ae13e845",
+    "url": "./static/js/6.d747cef0.chunk.js"
+  },
+  {
+    "revision": "1ab59cb48620e35ab841",
+    "url": "./static/js/7.650f038b.chunk.js"
+  },
+  {
+    "revision": "5aa69e7045e3774f9e9e",
+    "url": "./static/js/8.8d49b7c9.chunk.js"
+  },
+  {
+    "revision": "011b9ade218109fb61c6",
+    "url": "./static/js/9.3bcc81e3.chunk.js"
+  },
+  {
+    "revision": "210ddbb087a7574fb798",
+    "url": "./static/js/main.90b8eeec.chunk.js"
+  },
+  {
+    "revision": "2a0fb4770ac3a0b09c8b",
+    "url": "./static/js/runtime-main.c005ed7e.js"
+  },
+  {
+    "revision": "c03cd6d57e9a76bd28c033ff159fd714",
+    "url": "./static/media/OfficeCodePro-Bold.c03cd6d5.woff2"
+  },
+  {
+    "revision": "ed2eb92b8ef26aee9becaec22195e809",
+    "url": "./static/media/OfficeCodePro-Bold.ed2eb92b.woff"
+  },
+  {
+    "revision": "193d935889f1be2a6c1f2615ee2bafd7",
+    "url": "./static/media/OfficeCodePro-BoldItalic.193d9358.woff"
+  },
+  {
+    "revision": "9b5e744525f5bad182636a5ea296c1ad",
+    "url": "./static/media/OfficeCodePro-BoldItalic.9b5e7445.woff2"
+  },
+  {
+    "revision": "8a5df412ab55027bc19e284abffa0805",
+    "url": "./static/media/OfficeCodePro-Light.8a5df412.woff"
+  },
+  {
+    "revision": "ff1e3a1a8f9b6c64b2fb8a080be7c8b6",
+    "url": "./static/media/OfficeCodePro-Light.ff1e3a1a.woff2"
+  },
+  {
+    "revision": "027a6316722f6821fa5b86545428916b",
+    "url": "./static/media/OfficeCodePro-LightItalic.027a6316.woff"
+  },
+  {
+    "revision": "05ee9bc231e1d5c5659da57a0e01f948",
+    "url": "./static/media/OfficeCodePro-LightItalic.05ee9bc2.woff2"
+  },
+  {
+    "revision": "01e9be16e6a786b05098fd65bf138db9",
+    "url": "./static/media/OfficeCodePro-Medium.01e9be16.woff2"
+  },
+  {
+    "revision": "fa33df7924e33003bb3891ba7e7f6a17",
+    "url": "./static/media/OfficeCodePro-Medium.fa33df79.woff"
+  },
+  {
+    "revision": "4bbc62eca92289179eb7b1cad4b24602",
+    "url": "./static/media/OfficeCodePro-MediumItalic.4bbc62ec.woff2"
+  },
+  {
+    "revision": "e0d7ac010039fc0fc5a339d64deebba8",
+    "url": "./static/media/OfficeCodePro-MediumItalic.e0d7ac01.woff"
+  },
+  {
+    "revision": "568927fbb1f39b727ae03f4658933bca",
+    "url": "./static/media/OfficeCodePro-Regular.568927fb.woff"
+  },
+  {
+    "revision": "ad15ae29348234a8b0b041a0bc09f670",
+    "url": "./static/media/OfficeCodePro-Regular.ad15ae29.woff2"
+  },
+  {
+    "revision": "829ee48540af6a46f042e1218b35864e",
+    "url": "./static/media/OfficeCodePro-RegularItalic.829ee485.woff"
+  },
+  {
+    "revision": "c8fd5a9a92282c827dc1034952bd1d86",
+    "url": "./static/media/OfficeCodePro-RegularItalic.c8fd5a9a.woff2"
+  },
+  {
+    "revision": "67b67bb047ebb7765fcf3756ee157783",
+    "url": "./static/media/albania-logo.67b67bb0.svg"
+  },
+  {
+    "revision": "0ed673689b88a6bd07171128d1bbe7ad",
+    "url": "./static/media/data.0ed67368.svg"
+  },
+  {
+    "revision": "02e9c6474fd14c2616e86a24ef2afe17",
+    "url": "./static/media/download.02e9c647.svg"
+  },
+  {
+    "revision": "17645b381152456cc89b718e62bbe910",
+    "url": "./static/media/facebook.17645b38.svg"
+  },
+  {
+    "revision": "cb81eec0753fcfd0d1f3b131e22f1e07",
+    "url": "./static/media/growth-lab-white.cb81eec0.png"
+  },
+  {
+    "revision": "f0a47c0d3ae2da47b7516a606c714ce4",
+    "url": "./static/media/growth-lab.f0a47c0d.png"
+  },
+  {
+    "revision": "7315cdb4d6cf64e4b59a0f0a90916342",
+    "url": "./static/media/jordan-dotted-map.7315cdb4.svg"
+  },
+  {
+    "revision": "d97bab46858aa4b984ce86cab3837a9b",
+    "url": "./static/media/linkedin.d97bab46.svg"
+  },
+  {
+    "revision": "c03d99dfc4bb762c3db66aa59484e8da",
+    "url": "./static/media/scrolltop.c03d99df.svg"
+  },
+  {
+    "revision": "5cb7a42dc95a7789b7b05046039f03f9",
+    "url": "./static/media/twitter.5cb7a42d.svg"
+  }
+]);
