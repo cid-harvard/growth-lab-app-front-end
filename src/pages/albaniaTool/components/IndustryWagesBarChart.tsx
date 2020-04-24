2fd017ef89b23fe372c2e5fab7bc7bce68c83652
Kyle Soeltz <wsoeltz@gmail.com>
2020-04-24 12:11:37 -0400

    Add null/zero check for wages bar chart

--- src/pages/albaniaTool/components/IndustryWagesBarChart.tsx
+++ src/pages/albaniaTool/components/IndustryWagesBarChart.tsx
@@ -11,7 +11,25 @@ interface Props {
 
 export default (props: Props) => {
   const { industryWageEdge } = props;
-  if (industryWageEdge === null || industryWageEdge.node === null) {
+  console.log(industryWageEdge);
+  if (
+    industryWageEdge === null ||
+    industryWageEdge.node === null ||
+    (
+      !industryWageEdge.node.ind010k &&
+      !industryWageEdge.node.ind10k25k &&
+      !industryWageEdge.node.ind25k50k &&
+      !industryWageEdge.node.ind50k75k &&
+      !industryWageEdge.node.ind75k100k &&
+      !industryWageEdge.node.ind100kUp &&
+      !industryWageEdge.node.national010k &&
+      !industryWageEdge.node.national10k25k &&
+      !industryWageEdge.node.national25k50k &&
+      !industryWageEdge.node.national50k75k &&
+      !industryWageEdge.node.national75k100k &&
+      !industryWageEdge.node.national100kUp
+      )
+    ) {
     return (
       <DataViz
         id={'albania-industry-wages-bar-chart'}
