50609ad4ba889b4795a88643d26c36f9fa09c432
Kyle Soeltz <wsoeltz@gmail.com>
2020-05-18 16:57:55 -0400

    Add password protection to Jordan tool

---
+++ src/components/text/PasswordProtectedPage.tsx
@@ -0,0 +1,120 @@
+import React, {useState} from 'react';
+import styled from 'styled-components/macro';
+import {
+  lightBorderColor,
+  secondaryFont,
+} from '../../styling/styleUtils';
+import {lighten} from 'polished';
+
+const Root = styled.div`
+  background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAGCAYAAADgzO9IAAAAI0lEQVQYV2NkwAEYYeLeeU3GIPbWSXVnQTRhCXQT4TqIlgAACbAIB9ZyaUoAAAAASUVORK5CYII=) repeat;
+  padding: 1rem 2.5rem;
+  box-sizing: border-box;
+  grid-column: 1 / -1;
+  grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media(max-width: 900px) {
+    padding: 1rem;
+  }
+`;
+
+const Label = styled.p`
+  margin: 0 0 1rem;
+`;
+const LabelBackground = styled.span`
+  background-color: #fff;
+`;
+
+const PasswordContainer = styled.div`
+  display: flex;
+  margin-bottom: 1.5rem;
+`;
+
+const Password = styled.input`
+  font-size: 0.875rem;
+  padding: 0.5rem 0.75rem;
+  border: 1px solid ${lightBorderColor};
+  width: 300px;
+  max-width: 300px;
+  flex-grow: 0;
+  font-family: ${secondaryFont};
+
+  &::placeholder {
+    font-family: ${secondaryFont};
+  }
+
+  @media (max-width: 600px) {
+    width: auto;
+  }
+`;
+
+interface ButtonProps {
+  primaryColor: string;
+}
+
+const SubmitButton = styled.button<ButtonProps>`
+  text-align: center;
+  font-size: 0.875rem;
+  text-transform: uppercase;
+  height: 100%;
+  color: #fff;
+  padding: 0.45rem 0.75rem;
+  font-family: ${secondaryFont};
+  background-color: ${({primaryColor}) => primaryColor};
+  border: solid 1px ${({primaryColor}) => primaryColor};
+
+  &:hover {
+    background-color: ${({primaryColor}) => lighten(0.1 ,primaryColor)};
+    border-color: ${({primaryColor}) => lighten(0.1 ,primaryColor)};
+  }
+`;
+
+interface Props {
+  title: string;
+  buttonColor: string;
+  onPasswordSubmit: (value: string) => void;
+}
+
+const PasswordProtectedComponent = (props: Props) => {
+  const {
+    title, buttonColor, onPasswordSubmit,
+  } = props;
+
+  const [password, setPassword] = useState<string>('');
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onPasswordSubmit(password);
+  };
+
+  return (
+    <Root>
+      <Label>
+        <LabelBackground>
+          {title}
+        </LabelBackground>
+      </Label>
+      <PasswordContainer>
+        <form onSubmit={onSubmit}>
+          <Password
+            type='password'
+            placeholder='Type Your Password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <SubmitButton
+            primaryColor={buttonColor}
+          >
+            Submit
+          </SubmitButton>
+        </form>
+      </PasswordContainer>
+    </Root>
+  );
+};
+
+export default PasswordProtectedComponent;
