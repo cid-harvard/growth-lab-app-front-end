9d6e914c68f2e32ccebe3653adab3f115dcd3df5
Kyle Soeltz <wsoeltz@gmail.com>
2021-10-01 12:30:49 -0400

    Add dynamic text to Namibia tool shared/missing occupations table

--- src/pages/namibiaTool/components/SharedAndMissingOccupations.tsx
+++ src/pages/namibiaTool/components/SharedAndMissingOccupations.tsx
@@ -4,7 +4,11 @@ import {
   SectionHeaderSecondary,
 } from '../../../styling/styleUtils';
 import TextBlock from '../../../components/text/TextBlock';
-import {colorScheme} from '../Utils';
+import {
+  colorScheme,
+  useProductClass,
+  ProductClass,
+} from '../Utils';
 import DataViz, {
   VizType,
 } from 'react-fast-charts';
@@ -25,7 +29,9 @@ const SharedAndMissingOccupations = (props: Props) => {
     {label: 'Existing Occuptions', key: 'shared'},
     {label: 'Missing Occuptions', key: 'missing'},
   ];
-
+  const productClass = useProductClass();
+  const productOrIndustry = productClass === ProductClass.HS ? 'product' : 'industry';
+  const productOrIndustryPlural = productClass === ProductClass.HS ? 'products' : 'industries';
   const table = data.length ? (
     <DynamicTable
       columns={columns}
@@ -46,7 +52,7 @@ const SharedAndMissingOccupations = (props: Props) => {
         {table}
         <TextBlock>
           <div>
-            <p>The table to the left displays the top 10 existing and missing occupations present in Namibia that are associated with the products or other inputs used in the production of the product. Occupations related to this activity are ranked by those that comprise the biggest share of occupations in the activity and the table shows the top 10 for those occupations that exist and those that are missing in Namibia for this product.</p>
+            <p>The table to the left displays the top 10 existing and missing occupations present in Namibia that are associated with the {productOrIndustryPlural} or other inputs used in the production of the {productOrIndustry}. Occupations related to this activity are ranked by those that comprise the biggest share of occupations in the activity and the table shows the top 10 for those occupations that exist and those that are missing in Namibia for this {productOrIndustry}.</p>
           </div>
         </TextBlock>
       </TwoColumnSection>
