4f7674929325a01cf37844edbc79019cb527cf53
Kyle Soeltz <wsoeltz@gmail.com>
2021-10-01 12:14:49 -0400

    Update dynamic text for IndustryNow overview on Namibia tool

--- src/pages/namibiaTool/components/IndustryNow.tsx
+++ src/pages/namibiaTool/components/IndustryNow.tsx
@@ -5,21 +5,29 @@ import {
 import GeoMap, {Datum} from './GeoMap';
 import SharedAndMissingOccupations, {MissingSharedDatum} from './SharedAndMissingOccupations';
 import GroupsOfInterest, {BarDatum} from './GroupsOfInterest';
+import {
+  ProductClass,
+  useProductClass,
+} from '../Utils';
 
 interface Props {
+  industryName: string;
   heatMapData: Datum[];
   barData: BarDatum[];
   sharedMissingData: MissingSharedDatum[];
 }
 
 const IndustryNow = (props: Props) => {
-  const {heatMapData, barData, sharedMissingData} = props;
+  const { heatMapData, barData, sharedMissingData, industryName } = props;
+  const productClass = useProductClass();
+  const productOrIndustry = productClass === ProductClass.HS ? 'product' : 'industry';
+  const productOrIndustryPlural = productClass === ProductClass.HS ? 'products' : 'industries';
   return (
     <>
       <div id={'industry-now'}>
         <SectionHeader>Industry Now</SectionHeader>
         <p>
-          This section provides more information on the current presence of {'<<description>>'} in Namibia, including: the demand and market for this product in the region, the occupations that are present and missing in Namibia that are related to the product, and the employment of certain groups of interest within this product. Where data limitations do not allow for a description of this specific product, the information reflects the broader product classification of {'<<parent>>'}.
+          This section provides more information on the current presence of {industryName} in Namibia, including: the demand and market for this {productOrIndustry} in the region, the occupations that are present and missing in Namibia that are related to the {productOrIndustry}, and the employment of certain groups of interest within this {productOrIndustry}. The images in this section only display information for {productOrIndustryPlural} when data is available.
         </p>
       </div>
       <GeoMap
